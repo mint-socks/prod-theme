edc4e0eafbb66c9997889581d98f894c4fb5ca01
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-01-10 17:02:40 -0500

    create toggled expand collapse prod detail

---
+++ assets/mint.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     clickMe();
+//   });
+
+
+// // function clickMe() {
+// //   $('#cbp-pgcontainer').hover(function() {
+// //     msgMe()
+// //   })
+// // };
+
+// // function msgMe() {
+// //   alert('you clicked me again!')
+// // }
